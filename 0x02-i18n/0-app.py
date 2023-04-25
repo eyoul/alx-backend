116c994e247b404f601c42260d004b9dd53a4116
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-25 01:42:34 -0700

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello(name=None):
     return render_template('0-index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
