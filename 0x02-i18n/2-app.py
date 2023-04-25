2edf94300db90c47ba98b1eb7aeead77b67ea0d1
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-25 12:27:05 -0700

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""A Basic Flask app.
+"""
 from flask import Flask
 from flask_babel import Babel
 from flask import render_template, request
@@ -19,17 +21,18 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route('/')
-def get_index() -> str:
-    return render_template('2-index.html')
-
-
 @babel.localeselector
-def get_locale():
-    """determine the best match
-       with our supported languages.
+def get_locale() -> str:
+    """locale for a web page.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_index() -> str:
+    """he home/index page.
+    """
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
