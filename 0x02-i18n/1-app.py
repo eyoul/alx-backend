a0ce886eb62ea10d8c9c640f9679710e3d20c2aa
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-25 10:26:21 -0700

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+""" Basic Babel setup
+"""
 from flask import Flask
 from flask_babel import Babel
 from flask import render_template
 
 
 class Config:
+    """create a Config class that has a LANGUAGES
+       class attribute equal to ["en", "fr"].
+    """
     LANGUAGES = ["en", "fr" ]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
