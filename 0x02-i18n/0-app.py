b2f42023855861bd99e29b9c2989c09529c572eb
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-25 12:00:06 -0700

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello(name=None):
-    return render_template('0-index.html', name=name)
+def  get_index() -> str:
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
