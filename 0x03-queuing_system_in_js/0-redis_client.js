95216d5994da2ee318329e4e8110847deadd678e
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-24 08:48:46 -0700

    Update 0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,11 @@
-'use strict';
 import redis from 'redis';
+
 const client = redis.createClient();
 
-client.on("error", (error) => {
-  if (error) console.log(`Redis client not connected to the server: ${error}`)
-}).on('ready', () => {
-    console.log('Redis client connected to the server');
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
 });
